8480529b8bc2ea4a5a62c350ee70bc651da8577f
Celso Brito <202109526111@alunos.faculdadeideal.edu.br>
2022-09-27 14:23:24 -0300

    feat: Novo modelo de pontuação

--- web_app/quiz/modulos/pontuacoes.py
+++ web_app/quiz/modulos/pontuacoes.py
@@ -15,9 +15,8 @@ class Pontuacoes:
             -> Função utilizada para calcular a quantidade de estrelas baseada no teste de adequação a LPGD.
         :return: Não retorna nada.
         """
-        numero_de_perguntas = 8
 
-        self.__estrelas = (5 * self.__pontuacao_parcial) / numero_de_perguntas # Calculo das estrelas
+        self.__estrelas = 10 * self.__pontuacao_parcial# Calculo das estrelas
 
     # Função utilizada para calcular a pontuação parcial do usuário (100% = 2 pontos, 50% = 1 ponto)
     def calculo_pontuacao_parcial(self, lista_respostas) -> None:
@@ -25,29 +24,40 @@ class Pontuacoes:
             -> Função utilizada para calcular a pontuação parcial do usuário no teste de adequação
         :return: Não retorna nada
         """
-        for pos, c in enumerate(lista_respostas):  # Esse for anda pela lista respostas_qtd e também pelo seu indice
+        # Esse for anda pela lista respostas_qtd e também pelo seu indice
+        # a variável [c] significa o valor da resposta dada ao Quiz
+        for pos, c in enumerate(lista_respostas):  
             if c == 0: 
-                if self.perguntas_pesos.get(id=pos+1).pesos == 1:
-                    self.__pontuacao_parcial += 0
-                else:
-                    self.__pontuacao_parcial += 0
+                self.__pontuacao_parcial += 0
             elif c == 1:
-                if self.perguntas_pesos.get(id=pos+1).pesos == 1:
-                    self.__pontuacao_parcial += 0.25
-                else:
-                    self.__pontuacao_parcial += 0.50
-            elif c == 2: 
-                if self.perguntas_pesos.get(id=pos+1).pesos == 1:
-                    self.__pontuacao_parcial += 0.50
-                else:
-                    self.__pontuacao_parcial += 1
+                if self.perguntas_pesos.get(id=pos+1).pesos == 0.05:
+                    self.__pontuacao_parcial += 0.05 * 0.25 
+                elif self.perguntas_pesos.get(id=pos+1).pesos == 0.1:
+                    self.__pontuacao_parcial += 0.1 * 0.25 
+                elif self.perguntas_pesos.get(id=pos+1).pesos == 0.15:
+                    self.__pontuacao_parcial += 0.15 * 0.25 
+                elif self.perguntas_pesos.get(id=pos+1).pesos == 0.2:
+                    self.__pontuacao_parcial += 0.2 * 0.25
+            elif c == 2:
+                if self.perguntas_pesos.get(id=pos+1).pesos == 0.05:
+                    self.__pontuacao_parcial += 0.05 * 0.5 
+                elif self.perguntas_pesos.get(id=pos+1).pesos == 0.1:
+                    self.__pontuacao_parcial += 0.1 * 0.5 
+                elif self.perguntas_pesos.get(id=pos+1).pesos == 0.15:
+                    self.__pontuacao_parcial += 0.15 * 0.5 
+                elif self.perguntas_pesos.get(id=pos+1).pesos == 0.2:
+                    self.__pontuacao_parcial += 0.2 * 0.5 
             elif c == 3:
-                if self.perguntas_pesos.get(id=pos+1).pesos == 1:
-                    self.__pontuacao_parcial += 1
-                else:
-                    self.__pontuacao_parcial += 2
-
-    # Retorna o valor de estrelas
+                if self.perguntas_pesos.get(id=pos+1).pesos == 0.05:
+                    self.__pontuacao_parcial += 0.05 * 1 
+                elif self.perguntas_pesos.get(id=pos+1).pesos == 0.1:
+                    self.__pontuacao_parcial += 0.1 * 1 
+                elif self.perguntas_pesos.get(id=pos+1).pesos == 0.15:
+                    self.__pontuacao_parcial += 0.15 * 1 
+                elif self.perguntas_pesos.get(id=pos+1).pesos == 0.2:
+                    self.__pontuacao_parcial += 0.2 * 1 
+    
+    
     def get_estrelas(self):
         return self.__estrelas
 
--- web_app/quiz/static/quiz/js/script_resultado.js
+++ web_app/quiz/static/quiz/js/script_resultado.js
@@ -2,11 +2,23 @@
 let div_resultado = document.getElementById('resultado').innerHTML;
 div_resultado = div_resultado.substring(21, 26);
 
-if (parseFloat(div_resultado) < 3.5) {
-    const text = 'Você ainda não está no caminho da conformidade com a LGPD, entre em contato conosco e deixe sua empresa na frente.';
+if (parseFloat(div_resultado) < 2.99) {
+    const text = 'Iniciante';
     document.querySelector('.result-text').innerHTML = text;
 }
-else if (parseFloat(div_resultado) >= 3.5){
-    const text = 'Você entende o que é necessário para se alinhar com a LGPD, entre em contato conosco e deixe sua empresa na frente. ';
+else if ((parseFloat(div_resultado) >= 3.00) && (parseFloat(div_resultado) <= 4.99)){
+    const text = 'Básico';
+    document.querySelector('.result-text').innerHTML = text;
+}
+else if ((parseFloat(div_resultado) >= 5.00) && (parseFloat(div_resultado) <= 6.99)){
+    const text = 'Intermediário';
+    document.querySelector('.result-text').innerHTML = text;
+}
+else if ((parseFloat(div_resultado) >= 7.00) && (parseFloat(div_resultado) <= 8.99)){
+    const text = 'Intermediário Superior';
+    document.querySelector('.result-text').innerHTML = text;
+}
+else if (parseFloat(div_resultado) >= 9.00){
+    const text = 'Avançado';
     document.querySelector('.result-text').innerHTML = text;
 }
